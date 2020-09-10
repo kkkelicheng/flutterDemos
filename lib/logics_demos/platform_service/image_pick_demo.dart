b8e955e6d9c049eaf87eacceb8d1cf69e2b03b68
kelicheng <kelicheng131@gmail.com>
2020-09-10 18:11:44 +0800

    add some image

--- lib/logics_demos/platform_service/image_pick_demo.dart
+++ lib/logics_demos/platform_service/image_pick_demo.dart
@@ -39,9 +39,75 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
 
   Future getImage() async {
     PickedFile pickedFile = await picker.getImage(source: ImageSource.gallery);
+    
     print("get image : \n ${pickedFile.path}");
     setState(() {
       _image = File(pickedFile.path);
     });
   }
 }
+
+/*
+上传图片的组织formData的三种方式 dio.demo
+
+response = await dio.post(
+    //"/upload",
+    "http://localhost:3000/upload",
+    data: await FormData3(),
+    onSendProgress: (received, total) {
+      if (total != -1) {
+        print((received / total * 100).toStringAsFixed(0) + "%");
+      }
+    },
+  );
+  print(response);
+
+
+Future<FormData> FormData1() async {
+  return FormData.fromMap({
+    "name": "wendux",
+    "age": 25,
+    "file":
+        await MultipartFile.fromFile("./example/xx.png", filename: "xx.png"),
+    "files": [
+      await MultipartFile.fromFile("./example/upload.txt",
+          filename: "upload.txt"),
+      MultipartFile.fromFileSync("./example/upload.txt",
+          filename: "upload.txt"),
+    ]
+  });
+}
+
+Future<FormData> FormData2() async {
+  var formData = FormData();
+  formData.fields..add(MapEntry("name", "wendux"))..add(MapEntry("age", "25"));
+
+  formData.files.add(MapEntry(
+    "file",
+    await MultipartFile.fromFile("./example/xx.png", filename: "xx.png"),
+  ));
+
+  formData.files.addAll([
+    MapEntry(
+      "files[]",
+      await MultipartFile.fromFile("./example/upload.txt",
+          filename: "upload.txt"),
+    ),
+    MapEntry(
+      "files[]",
+      MultipartFile.fromFileSync("./example/upload.txt",
+          filename: "upload.txt"),
+    ),
+  ]);
+  return formData;
+}
+
+Future<FormData> FormData3() async {
+  return FormData.fromMap({
+    "file": await MultipartFile.fromFile("./example/upload.txt",
+        filename: "uploadfile"),
+  });
+}
+
+
+*/
